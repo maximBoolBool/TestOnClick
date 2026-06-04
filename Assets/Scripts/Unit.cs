cd24703c5f7956c874b03ffe7169f9cd1926f4f5
maximBoolBool <remezmax117@gmail.com>
2026-06-04 23:22:31 +0300

    PROTOTYPE-5

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -9,42 +9,42 @@ using Assets.Scripts.Models.Slot;
 namespace Assets.Scripts
 {
     public class Unit : MonoBehaviour
-{
-    public string Name { get; set; }
-    public int ActualActionPoints { get; set; }
-    public int ActualHealthPoints { get; set; }
-    public bool IsSelected { get; set; }
-    public UnitCharacteristic Characterictics;
-    public List<BaseAction> Actions { get; set; } = new();
-    public List<EquipmentSlot> EqupmentSlots = new();
-    public List<BaseCondition> GlobalConditions { get; set; } = new();
-    public List<(BaseCondition Condition, int DisappearancesTurn)> DuratationConditions { get; set; } = new();
-    // TODO: Consider adding validation - checking for values less than zero
-    public bool IsDead => ActualHealthPoints <= 0;
-}
-
-public static class UnitExtension
-{
-    public static Unit SetCharacterictics(this Unit unit,  UnitEntity entity)
     {
-        unit.Name = entity.Name;
-        unit.ActualHealthPoints = entity.HealthPoints;
-        unit.ActualActionPoints = entity.ActiveActionPoints;
-
-        unit.Characterictics = new UnitCharacteristic()
-        {
-            HealthPoints = entity.HealthPoints,
-            ActiveActionPoints = entity.ActiveActionPoints,
-            ReactionActionPoints = entity.ReactionActionPoints,
-            Agility = entity.Agility,
-            MeleeSkill = entity.MeleeSkill,
-            DefendSkill = entity.DefendSkill,
-            Side = entity.Side
-        };
-
-        return unit;
+        public string Name { get; set; }
+        public int ActualActionPoints { get; set; }
+        public int ActualHealthPoints { get; set; }
+        public bool IsSelected { get; set; }
+        public UnitCharacteristic Characterictics;
+        public List<BaseAction> Actions { get; set; } = new();
+        public List<EquipmentSlot> EqupmentSlots = new();
+        public List<BaseCondition> GlobalConditions { get; set; } = new();
+        public List<(BaseCondition Condition, int DisappearancesTurn)> DuratationConditions { get; set; } = new();
+        // TODO: Consider adding validation - checking for values less than zero
+        public bool IsDead => ActualHealthPoints <= 0;
     }
-}
+
+    public static class UnitExtension
+    {
+        public static Unit SetCharacterictics(this Unit unit,  UnitEntity entity)
+        {
+            unit.Name = entity.Name;
+            unit.ActualHealthPoints = entity.HealthPoints;
+            unit.ActualActionPoints = entity.ActiveActionPoints;
+
+            unit.Characterictics = new UnitCharacteristic()
+            {
+                HealthPoints = entity.HealthPoints,
+                ActiveActionPoints = entity.ActiveActionPoints,
+                ReactionActionPoints = entity.ReactionActionPoints,
+                Agility = entity.Agility,
+                MeleeSkill = entity.MeleeSkill,
+                DefendSkill = entity.DefendSkill,
+                Side = entity.Side
+            };
+
+            return unit;
+        }
+    }
 
     public class PriorityQueue<T>
     {
