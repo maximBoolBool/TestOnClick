3ea398e45c68ef2e8d8a4dd9de8427f557986b0f
maximBoolBool <remezmax117@gmail.com>
2026-08-19 00:48:24 +0300

    PRT-10

--- Assets/Scripts/Services/HitService.cs
+++ Assets/Scripts/Services/HitService.cs
@@ -20,11 +20,11 @@ namespace Assets.Scripts.Services
 
     public class HitService : IHitService
     {
-        public const int MaxChanceToHit = 95;
-        public const int MinChanceToHit = 5;
+        public const int MAX_CHANCE_TO_HIT = 95;
+        public const int MIN_CHANCE_TO_HIT = 5;
 
-        public const int MaxRandomValue = 100;
-        public const int MinRandomValue = 1;
+        public const int MAX_RANDOM_VALUE = 100;
+        public const int MIN_RANDOM_VALUE = 1;
 
         public bool IsHit(
             int defendSkill,
@@ -39,7 +39,7 @@ namespace Assets.Scripts.Services
             );
 
             var random = new Random();
-            var roll = random.Next(MinRandomValue, MaxRandomValue);
+            var roll = random.Next(MAX_RANDOM_VALUE, MIN_RANDOM_VALUE);
 
             var result = roll <= chance;
             return result;
@@ -53,7 +53,7 @@ namespace Assets.Scripts.Services
         )
         {
             var chance = attckSkill - defendSkill;
-            return Math.Clamp(chance, MinChanceToHit, MaxChanceToHit);
+            return Math.Clamp(chance, MAX_CHANCE_TO_HIT, MIN_CHANCE_TO_HIT);
         }
     }
 
